a4b83c26d86d5d218644d2dfd15f2130300e41e2
Preetam Shri <preetamsshri@gmail.com>
2023-06-03 11:05:45 +0530

    responsive searchbar setup

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ class App extends React.Component {
     };
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
+    const filterRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>Robofriends</h1>
-        <SearchBox />
-        <Cardlist />
+        <SearchBox searchChange={this.onSearchChange} />
+        <Cardlist robots={filterRobots} />
       </div>
     );
   }
